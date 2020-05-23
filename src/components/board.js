768706b89a228b4952ebd554232f426a80a13b22
Ostretsova Anna <ostretsova.ann@gmail.com>
2020-05-23 19:44:54 +0300

    рефакторинг

--- src/components/board.js
+++ src/components/board.js
@@ -5,7 +5,6 @@ const createBoardTemplate = () => {
     `<section class="films">
     <section class="films-list">
       <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-
     </section>
   </section>`
   );
